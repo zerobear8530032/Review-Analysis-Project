20d583fd6b333a73573cc0b303c3898b71ea4518
=zerobear8530032 <=zerobear8530032@gmail.com>
2024-12-03 18:32:39 +0530

    fix some bugs

--- reviewanalysis/models.py
+++ reviewanalysis/models.py
@@ -11,7 +11,6 @@ class Registertable(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     api_count = db.Column(db.Integer, default=0)
     
