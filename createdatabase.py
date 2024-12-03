461c3d3235d8838a13191c7f43c74dca97d1eac3
=zerobear8530032 <=zerobear8530032@gmail.com>
2024-12-03 18:45:12 +0530

    fix create data base

--- createdatabase.py
+++ createdatabase.py
@@ -2,12 +2,5 @@ from reviewanalysis import app, db
 from reviewanalysis.models import APItable
 
 
-# # Inside the Python shell or script where you need to perform database operations:
-# db.create_all()
-# print(db.__repr__)
-key_record = APItable.query.all("api_key").first()
-print(key_record)
-# try:
-#     print(f"{key_record.api_key}    {key_record.usage_count}")
-# except Exception as e:
-#     print(e)
+db.create_all()
+print(db.__repr__)
